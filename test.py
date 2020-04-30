f82d3311ab50622a30243a9cccefc5865e4aba84
Alexandra Huchzermeier <AlexandraHuchzermeier@AlexandeiersMBP.fritz.box>
2020-04-30 11:19:48 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Web Page</title>
+    </head>
+    <body>
+        <h2> Please enter your name </h2>
+        <input type="text" placeholder="Full Name" name="name">
+        <button>Submit!</button>
+    </body>
+</html>
